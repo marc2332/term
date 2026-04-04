8e97426e59ca546a6f423395f3831df332838ed6
marc2332 <marc@mespin.me>
2026-04-04 18:29:22 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,109 @@
+{
+  description = "marcterm - A terminal emulator built with Freya and Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        rpathLibs = with pkgs; [
+          libGL
+          libxkbcommon
+          wayland
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXi
+          xorg.libxcb
+          fontconfig
+          freetype
+          stdenv.cc.cc.lib
+        ];
+
+        marcterm = pkgs.rustPlatform.buildRustPackage {
+          pname = "marcterm";
+          version = "0.1.14";
+
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+
+          # Skia downloads sources during build, needs network access
+          __noChroot = true;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            cmake
+            python3
+            makeWrapper
+            curl
+            cacert
+            git
+          ];
+
+          buildInputs = with pkgs; [
+            fontconfig
+            freetype
+            libxkbcommon
+            libGL
+            wayland
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libxcb
+            systemd
+          ];
+
+          # Required for Skia download during build
+          SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+          GIT_SSL_CAINFO = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+
+          postInstall = ''
+            install -Dm644 flatpak/io.marc.term.desktop \
+              $out/share/applications/io.marc.term.desktop
+            install -Dm644 icon.png \
+              $out/share/icons/hicolor/128x128/apps/io.marc.term.png
+            install -Dm644 flatpak/io.marc.term.metainfo.xml \
+              $out/share/metainfo/io.marc.term.metainfo.xml
+          '';
+
+          postFixup = ''
+            patchelf --set-rpath "${pkgs.lib.makeLibraryPath rpathLibs}" \
+              $out/bin/marcterm
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Terminal emulator built with Freya and Rust";
+            homepage = "https://github.com/marc2332/marcterm";
+            license = licenses.mit;
+            platforms = platforms.linux;
+            mainProgram = "marcterm";
+          };
+        };
+      in {
+        packages.default = marcterm;
+        packages.marcterm = marcterm;
+
+        apps.default = {
+          type = "program";
+          program = "${marcterm}/bin/marcterm";
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ marcterm ];
+          packages = with pkgs; [ rust-analyzer clippy rustfmt ];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath rpathLibs;
+        };
+      }
+    ) // {
+      overlays.default = final: prev: {
+        marcterm = self.packages.${prev.system}.marcterm;
+      };
+    };
+}
